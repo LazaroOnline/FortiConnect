cd091e65719f5b3e71d366eb238c76bfd27fc8e7
Lazaro <lazaroonline@gmail.com>
2020-12-09 22:54:11 +0100

    Target dotnet 5.0.

--- src/Services/EmailService.cs
+++ src/Services/EmailService.cs
@@ -17,10 +17,10 @@ namespace FortiConnect.Services
 		{
 			switch(protocol) {
 				default:
-				case EmailServerProtocol.Exchange: return new EmailExchangeProvider();    break; // Splat.Locator.Current.GetService<EmailExchangeProvider>();
-				case EmailServerProtocol.Imap:     return new EmailImapMailkitProvider(); break; // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
-				case EmailServerProtocol.Pop3:     return new EmailImapMailkitProvider(); break; // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
-				case EmailServerProtocol.Smtp:     return new EmailImapMailkitProvider(); break; // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
+				case EmailServerProtocol.Exchange: return new EmailExchangeProvider();    // Splat.Locator.Current.GetService<EmailExchangeProvider>();
+				case EmailServerProtocol.Imap:     return new EmailImapMailkitProvider(); // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
+				case EmailServerProtocol.Pop3:     return new EmailImapMailkitProvider(); // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
+				case EmailServerProtocol.Smtp:     return new EmailImapMailkitProvider(); // Splat.Locator.Current.GetService<EmailImapMailkitProvider>();
 			}
 		}
 	}
