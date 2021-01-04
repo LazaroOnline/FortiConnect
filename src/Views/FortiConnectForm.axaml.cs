d862b5ebe07942d1fa3fbc2f0b25e81da0a8c9e7
Lazaro <lazaroonline@gmail.com>
2021-01-04 05:00:30 +0100

    Separate window from form.

---
+++ src/Views/FortiConnectForm.axaml.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using FortiConnect.ViewModels;
+
+namespace FortiConnect.Views
+{
+	public class FortiConnectForm : UserControl
+	{
+		public FortiConnectForm()
+		{
+			this.InitializeComponent();
+			//this.OnClosing += Closing;
+			
+			var vpnPasswordTextBox = this.FindControl<TextBox>("VpnPassword");
+			var toggleShowVpnPasswordButton = this.FindControl<Button>("ToggleShowVpnPassword");
+			var vpnPasswordHiddenChar = vpnPasswordTextBox.PasswordChar;
+			toggleShowVpnPasswordButton.Click += (o, e) => {
+				var isPasswordHidden = vpnPasswordTextBox.PasswordChar == vpnPasswordHiddenChar;
+				if (isPasswordHidden)
+					vpnPasswordTextBox.PasswordChar = '\0' ;
+				else
+					vpnPasswordTextBox.PasswordChar = vpnPasswordHiddenChar;
+			};
+			
+			var emailPasswordTextBox = this.FindControl<TextBox>("EmailPassword");
+			var toggleShowEmailPasswordButton = this.FindControl<Button>("ToggleShowEmailPassword");
+			var emailPasswordHiddenChar = emailPasswordTextBox.PasswordChar;
+			toggleShowEmailPasswordButton.Click += (o, e) => {
+				var isPasswordHidden = emailPasswordTextBox.PasswordChar == emailPasswordHiddenChar;
+				if (isPasswordHidden)
+					emailPasswordTextBox.PasswordChar = '\0' ;
+				else
+					emailPasswordTextBox.PasswordChar = emailPasswordHiddenChar;
+			};
+			
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+		
+		// TODO: try to bind "OnCloseAboutPopup" directly from XAML instead from this code-behind:
+		public void AboutViewExitHandler(object sender, RoutedEventArgs e)
+		{
+			var viewModel = (MainWindowViewModel)this.DataContext;
+			viewModel.IsAboutVisible = false;
+			e.Handled=true;
+		}
+
+	}
+}
--- src/Views/MainWindow.axaml.cs
+++ src/Views/MainWindow.axaml.cs
@@ -4,7 +4,6 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using FortiConnect.ViewModels;
-using Avalonia.Interactivity;
 
 namespace FortiConnect.Views
 {
@@ -17,30 +16,6 @@ namespace FortiConnect.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
-			//this.OnClosing += Closing;
-			
-			var vpnPasswordTextBox = this.FindControl<TextBox>("VpnPassword");
-			var toggleShowVpnPasswordButton = this.FindControl<Button>("ToggleShowVpnPassword");
-			var vpnPasswordHiddenChar = vpnPasswordTextBox.PasswordChar;
-			toggleShowVpnPasswordButton.Click += (o, e) => {
-				var isPasswordHidden = vpnPasswordTextBox.PasswordChar == vpnPasswordHiddenChar;
-				if (isPasswordHidden)
-					vpnPasswordTextBox.PasswordChar = '\0' ;
-				else
-					vpnPasswordTextBox.PasswordChar = vpnPasswordHiddenChar;
-			};
-			
-			var emailPasswordTextBox = this.FindControl<TextBox>("EmailPassword");
-			var toggleShowEmailPasswordButton = this.FindControl<Button>("ToggleShowEmailPassword");
-			var emailPasswordHiddenChar = emailPasswordTextBox.PasswordChar;
-			toggleShowEmailPasswordButton.Click += (o, e) => {
-				var isPasswordHidden = emailPasswordTextBox.PasswordChar == emailPasswordHiddenChar;
-				if (isPasswordHidden)
-					emailPasswordTextBox.PasswordChar = '\0' ;
-				else
-					emailPasswordTextBox.PasswordChar = emailPasswordHiddenChar;
-			};
-			
 		}
 
 		private void InitializeComponent()
@@ -51,16 +26,9 @@ namespace FortiConnect.Views
 		protected override void OnClosing(CancelEventArgs e)
 		{
 			base.OnClosing(e);
-			var viewModel = (MainWindowViewModel)this.DataContext;
+			var fortiConnectForm = this.FindControl<FortiConnectForm>("FortiConnectForm");
+			var viewModel = (MainWindowViewModel)fortiConnectForm.DataContext;
 			viewModel.SaveConfig();
-		}
-
-		// TODO: try to bind "OnCloseAboutPopup" directly from XAML instead from this code-behind:
-		public void AboutViewExitHandler(object sender, RoutedEventArgs e)
-		{
-			var viewModel = (MainWindowViewModel)this.DataContext;
-			viewModel.IsAboutVisible = false;
-			e.Handled=true;
 		}
 
 	}
