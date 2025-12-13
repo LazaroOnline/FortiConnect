38792f7938445b77ed828b970d0d2ba94d478490
Lazaro <lazaroonline@gmail.com>
2025-12-13 16:47:42 +0100

    Add build script.

---
+++ scripts/Build.ps1
@@ -0,0 +1,52 @@
+# BUILD APP
+
+# Navitage to the solution folder:
+$scriptFolder = if ($PSScriptRoot -eq "") { "." } else { $PSScriptRoot } # Support PSv5/Core running from script file or copy/pasted into a prompt.
+cd "$scriptFolder/.."
+Write-Host -F Blue "Changed dir to: '$PWD'"
+
+
+# CONFIG:
+$csprojPath = "src/FortiConnect.csproj"
+$appFileName = "FortiConnect" # .exe
+$publishFolder = "Publish"
+$publishFolderApp = "$publishFolder/app"
+
+
+# CLEANUP:
+Write-Host -F Blue "Cleaning up folders: '$publishFolderApp' and '$publishFolderDotnetTool'..."
+if (test-path $publishFolderApp) { Remove-Item "$publishFolderApp/*" -Force -Recurse; Write-Host -F Yellow "Removed folder: $publishFolderApp" }
+Write-Host -F Blue "Starting compilation..."
+
+
+# COMPILE AS EXE:
+# https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
+Write-Host -F Blue "Publishing app..."
+dotnet publish $csprojPath `
+--self-contained false `
+-o $publishFolderApp `
+-c "Release" `
+-p:DebugType=None `
+-p:EnableCompressionInSingleFile=false `
+-p:PublishTrimmed=false `
+-p:PublishSingleFile=false `
+-p:PublishAot=false
+# -r "win-x64" 
+$compiledAppFile = Get-Item "$publishFolderApp/$appFileName.exe" -ErrorAction SilentlyContinue
+if ($compiledAppFile -eq $null) {
+	Write-Host -F Red "Error compiling/publishing ('$appFileName.exe' not created)."
+	return
+}
+Write-Host -F Green "Publishing app DONE!"
+
+
+# COMPRESS TO ZIP:
+$version = $compiledAppFile.VersionInfo.FileVersion
+$destinationZip = "$publishFolder/$appFileName-v$version.zip"
+Write-Host -F Blue "Compressing binaries into: '$destinationZip'..."
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/compress-archive?view=powershell-7.3
+Compress-Archive -Path "$publishFolderApp/*" -DestinationPath $destinationZip -Force
+
+
+Start $publishFolder
+Write-Host -F Green "Finished Build!"
